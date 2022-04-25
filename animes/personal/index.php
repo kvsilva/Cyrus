d3aba429adc3d77908b3b9da22aa23dfd91fadb8
Kurookami <vesilva24a@gmail.com>
2022-04-25 16:05:46 +0100

    Anime Page - updated

--- animes/personal/index.php
+++ animes/personal/index.php
@@ -8,6 +8,7 @@ $files = array(
     "jquery" => (Utils::getURL() . "/resources/dependencies/JQuery/jquery-3.6.0.min.js"),
     "bootstrap_js" => (Utils::getURL() . "/resources/dependencies/Bootstrap 5/js/bootstrap.bundle.min.js"),
     "bootstrap_css" => (Utils::getURL() . "/resources/dependencies/Bootstrap 5/css/bootstrap.min.css"),
+    "popper_js" => (Utils::getURL() . "/resources/dependencies/Popper/popper.min.js"),
     "fontawesome_css" => (Utils::getURL() . "/resources/dependencies/fontawesome-6.1.1/css/all.css"),
     "personal_css" => (Utils::getURL() . "/animes/personal/assets/css/personal.css"),
     "personal_js" => (Utils::getURL() . "/animes/personal/assets/js/personal.js"),
@@ -23,6 +24,8 @@ $files = array(
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- JQuery -->
     <script src="<?php echo $files['jquery']; ?>"></script>
+    <!-- Popper -->
+    <script src="<?php echo $files['popper_js']; ?>"></script>
     <!-- Bootstrap -->
     <link href="<?php echo $files['bootstrap_css']; ?>" rel="stylesheet">
     <script src="<?php echo $files['bootstrap_js']; ?>"></script>
@@ -61,7 +64,7 @@ $files = array(
                     <span>70 vídeos</span>
                     <span>70 algo</span>
                 </div>
-                <div id="rating">
+                <div class="rating" id = "rating-average">
                     <i class="fa-solid fa-star star"></i><i class="fa-solid fa-star star"></i><i class="fa-solid fa-star star"></i><i class="fa-solid fa-star star"></i><i class="fa-solid fa-star star"></i>
                 </div>
                 <div id="synopsis">
@@ -148,7 +151,6 @@ $files = array(
                 <div class = "reviews-average-rating">
                     <span id="reviews-average-rating_value">12 Críticas </span>
                 </div>
-
                 <div class = "reviews-count">
                     <span id="reviews-average-count_value">4.9 <i class="fa-solid fa-star"></i> (45.2k)</span>
                 </div>
@@ -180,6 +182,106 @@ $files = array(
                     </div>
                 </div>
             </div>
+            <div class = "reviews-section">
+                <div class = "review-post mt-3">
+                    <div class = "row">
+                        <div class = "col-2 review-post-user no select">
+                            <img draggable="false" class = "mx-auto" src = "https://static.crunchyroll.com/assets/avatar/170x170/1044-jujutsu-kaisen-satoru-gojo.png">
+                            <span class = "review-post-username">Kurookami</span>
+                        </div>
+                        <div class = "col-9">
+                            <div class = "review-post-rating">
+                                <div class = "rating" style = "position: relative;" >
+                                    <span class = "reviews-classified-as">Classificaste como 5 Estrelas</span><?php
+                                    for($i = 5; $i > 0; $i--){
+                                        $text = "Classificar com " . $i . ($i == 1 ? " estrela" : " estrelas");
+                                        ?>
+                                        <i data-bs-toggle="tooltip" data-bs-placement="bottom" title="<?php echo $text?>" class="fa-solid fa-star star"></i>
+                                    <?php
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                            <form class = "cyrus-form">
+                                <div class = "cyrus-form-inputs">
+                                    <label class = "cyrus-label">
+                                        <input class = "cyrus-input" type ="text" placeholder="Título">
+                                    </label>
+                                    <div class = "reviews-self-char-notification"><span>Mínimo de 8 caracteres</span></div>
+                                    <label class = "cyrus-label">
+                                        <textarea class = "cyrus-input reviews-self-textarea" placeholder="Descrição"></textarea>
+                                    </label>
+                                    <div class = "reviews-self-char-notification"><span>0/200 caracteres</span></div>
+                                    <label class = "cyrus-label-checkbox mt-2">
+                                        <span class = "cyrus-hover-pointer">
+                                            <input class = "cyrus-input-checkbox-null" type = "checkbox">
+                                            <span class="cyrus-input-checkbox-checkmark"></span>
+                                            <span>Marcar como Spoiler</span>
+                                        </span>
+                                    </label>
+                                </div>
+                                <div class = "cyrus-form-buttons">
+                                    <input data-toggle="tooltip" data-placement="top" title="Tooltip on top" class = "cyrus-input" type = "reset" value="CANCELAR">
+                                    <input class = "cyrus-input" type = "submit" value = "PUBLICAR">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div id = "reviews-list" class = "mt-3">
+                    <?php
+                    for($x = 0; $x < 5; $x++){
+
+                    ?>
+                    <div class = "review">
+                        <div class = "row">
+                            <div class = "col-2 review-post-user no-select">
+                                <img draggable="false" class = "mx-auto" src = "https://static.crunchyroll.com/assets/avatar/170x170/1044-jujutsu-kaisen-satoru-gojo.png">
+                            </div>
+                            <div class = "col-9">
+                                <span>
+                                    <span class = "review-username">Kurookami</span>
+                                    <span class = "review-date float-right">10 de Janeiro de 2021</span>
+                                </span>
+                                <span class = "review-options">
+                                    <button class = "cyrus-btn cyrus-btn-simple"><i class="fa-solid fa-flag"></i></button>
+                                    <button class = "cyrus-btn cyrus-btn-simple"><i class="fa-solid fa-share-nodes"></i></button>
+                                </span>
+                                <div class = "review-star mt-3">
+                                    <?php
+                                    for($i = 0; $i < 5; $i++){ ?>
+                                        <i class="fa-solid fa-star star static filled"></i>
+                                    <?php
+                                    }
+                                    ?>
+                                </div>
+                                <div class = "mt-3">
+                                    <h3 class = "review-title">Lorem ipsum dolor sit amet</h3>
+                                    <div class = "review-description ">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla nunc at arcu rhoncus facilisis. Donec at justo eget eros auctor porttitor ut in magna. Etiam porta commodo dolor. Sed a enim dapibus, placerat erat sit amet, rhoncus ipsum. Fusce ut lobortis turpis, a hendrerit leo. Vivamus dui ipsum, tristique vulputate vulputate nec, cursus non enim. Proin molestie ante a lorem congue, quis tincidunt ligula consectetur. Sed id tempus mi, sed finibus nulla.
+                                            Curabitur sodales viverra dapibus. Aenean fermentum dui turpis, non consectetur sapien posuere in. Duis gravida vitae arcu sed rhoncus. Integer vel ex dapibus, dapibus dolor vel, tincidunt mi. Nullam eget suscipit lorem. Integer a nibh non purus aliquam efficitur. Nullam consequat condimentum nulla, vitae mollis ipsum dignissim sit amet. Suspendisse potenti. Praesent tristique dolor mauris, a suscipit sem ultricies ut.
+                                            Suspendisse fermentum erat nunc, consequat mattis dolor posuere nec. Vivamus pretium in ligula in dapibus. Nulla facilisi. Donec lectus ligula, sagittis eu tincidunt eget, aliquam a mauris. Maecenas et purus luctus, pretium tellus ac, aliquet augue. Phasellus sollicitudin justo sit amet ligula vulputate, eget vehicula orci rutrum. Phasellus placerat rhoncus convallis. Curabitur eleifend, justo sed tempus finibus, neque nulla varius urna, sit amet ultrices urna metus in nibh. Sed sed sodales urna, nec pretium orci. Phasellus rhoncus ac nisl id lobortis. Morbi sit amet elit laoreet, viverra dui sit amet, efficitur nunc. Nulla cursus ante id tempor sodales.
+                                            Fusce luctus lacus libero. Integer bibendum lacinia urna, id faucibus ipsum hendrerit ut. Fusce bibendum tellus sit amet accumsan malesuada. Nam facilisis nibh vestibulum ex condimentum, ut lobortis ex pharetra. Mauris porta tristique cursus. Duis cursus magna id iaculis ornare. Duis ultrices nunc nisl, nec porttitor est volutpat ut. Vestibulum non congue metus, tempor consequat tellus. Proin vitae ex a nisi volutpat fringilla. Vivamus sit amet consequat ante, in dignissim magna. Nullam vitae lobortis ligula, a sodales tellus. Sed luctus risus id interdum efficitur.
+                                            Vivamus euismod ipsum quis facilisis congue. Proin et tincidunt velit. Quisque sit amet porta metus. Sed non eros ut diam consectetur rhoncus. Nam at dui lacus. Quisque nibh mi, bibendum sit amet nunc nec, imperdiet euismod leo. Mauris blandit odio eleifend nisi aliquet maximus laoreet non arcu.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <button class = "cyrus-btn cyrus-btn-simple">MOSTRAR MAIS</button>
+                                </div>
+                                <div class ="evaluate-review mt-2">
+                                    <span data-positive="86">86</span> de <span data-total = "100">100</span> pessoas consideraram esta crítica útil. É útil para si? <button class = "cyrus-btn cyrus-btn-simple evaluate-review-button">SIM</button> | <button class = "cyrus-btn cyrus-btn-simple">NÃO</button>
+                                </div>
+                            </div>
+                        </div>
+                        <!--<hr class = "w-25 mx-auto">-->
+                    </div>
+                    <?php
+                    }
+                    ?>
+
+                </div>
+            </div>
         </div>
         <div class = "toDELETE">
             <?php
@@ -194,6 +296,12 @@ $files = array(
 <footer>
 
 </footer>
-
+<script>
+    // Initialize tooltips
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+</script>
 </body>
 </html>
